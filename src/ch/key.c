3b259a429806b9cf3be39b3e6a29f5a939543689
alies_a <arnaud.alies@epitech.eu>
2016-05-09 16:50:46 +0200

    ok

--- src/ch/key.c
+++ src/ch/key.c
@@ -5,12 +5,39 @@
 ** Login   <alies_a@epitech.net>
 ** 
 ** Started on  Mon May  9 16:29:52 2016 alies_a
-** Last update Mon May  9 16:35:01 2016 alies_a
+** Last update Mon May  9 16:49:58 2016 alies_a
 */
+
+#include <termios.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
 
 char up[] = {91, 65};
 
-int	ch_key()
+int	ch_read_key()
 {
-  
+  char	*comb;
+  char	buff;
+
+  read(STDIN_FILENO, &buff, 1);
+  return (5);
+}
+
+int			ch_key()
+{
+  struct termios	term;
+  struct termios	old_term;
+  int			res;
+
+  if (ioctl(STDIN_FILENO, TCGETS, &term) == -1)
+    return (-1);
+  old_term = term;
+  term.c_cc[VMIN] = 0;
+  term.c_cc[VTIME] = 10;
+  if (ioctl(STDIN_FILENO, TCSETS, &term) == -1)
+    return (-1);
+  res = ch_read_key();
+  if (ioctl(STDIN_FILENO, TCSETS, &old_term) == -1)
+    return (-1);
+  return (res);
 }
