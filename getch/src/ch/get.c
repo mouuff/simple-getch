ee190aaae48b4b17017c71cf7dce910596937606
alies_a <arnaud.alies@epitech.eu>
2016-05-19 15:36:24 +0200

    norm

--- getch/src/ch/get.c
+++ getch/src/ch/get.c
@@ -5,7 +5,7 @@
 ** Login   <alies_a@epitech.net>
 ** 
 ** Started on  Mon May  9 16:18:41 2016 alies_a
-** Last update Mon May 16 14:02:22 2016 alies_a
+** Last update Thu May 19 15:36:15 2016 alies_a
 */
 
 #include <unistd.h>
@@ -14,7 +14,7 @@
 int		ch_get(t_key const *keys)
 {
   char		buff;
-  
+
   if (read(STDIN_FILENO, &buff, 1) != 1)
     return (K_ERR);
   if (buff == CH_ESC)
